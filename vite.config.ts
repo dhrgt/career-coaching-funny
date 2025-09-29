95c4614aa2f16cfd97ecce518f94e68fd582fbef
Dhruv Gupta <dhruvgupta@Olivias-MBP.lan>
2025-09-29 01:23:04 -0400

    Fix Vite base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/career-coaching-funny/',  // repo name
+  base: '/career-coaching-funny/',   // REQUIRED for repo-based GitHub Pages
 })
