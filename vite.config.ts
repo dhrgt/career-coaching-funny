04e146fc75b688dce35567ac2e9b2227956de62e
Dhruv Gupta <32078122+dhrgt@users.noreply.github.com>
2025-09-29 01:28:55 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/career-coaching-funny/',   // REQUIRED for repo-based GitHub Pages
+  base: '/career-coaching-funny/',  // 👈 repo name
 })
